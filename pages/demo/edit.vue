5de141afaba10d0f0116ad8dd9d496c9cbf1fbe9
berryamo <9kamome@gmail.com>
2023-07-22 00:25:15 +0900

    feat: デモ画面作成

---
+++ pages/demo/edit.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <h1>increment</h1>
+    <p>Count: {{ count }}</p>
+    <button @click="increment">Increment</button>
+  </div>
+</template>
+
+<script>
+import { useCounterStore } from "~/store/counter";
+
+export default {
+  data() {
+    return {
+      store: useCounterStore(),
+    };
+  },
+  computed: {
+    count() {
+      return this.store.count;
+    },
+  },
+  methods: {
+    increment() {
+      this.store.increment();
+    },
+  },
+};
+</script>
---
+++ pages/demo/preview.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h1>preview</h1>
+    <p>Count: {{ count }}</p>
+  </div>
+</template>
+
+<script>
+import { useCounterStore } from "~/store/counter";
+
+export default {
+  data() {
+    return {
+      store: useCounterStore(),
+    };
+  },
+  computed: {
+    count() {
+      return this.store.count;
+    },
+  }
+};
+</script>
